96736a0b4ce40cdf4ca927419ca505c3db123da2
v.kulchynski <v.kulchynski@nord-soft.com>
2023-06-05 15:10:44 +0300

    update admin.py

--- sft/app/admin.py
+++ sft/app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Contract, Bid, Manufacturer, Product
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
